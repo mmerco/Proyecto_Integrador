51fa4f4debb8c3fb5ec3c01ff1fab8694d47caa1
Mariano Mercogliano <mercogliano@gmail.com>
2023-11-18 19:42:17 -0300

    [FEAT] rutas y controladores para shop

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mainRoutes from './src/routes/mainRoutes.js';
 import authRoutes from './src/routes/authRoutes.js';
+import shopRoutes from './src/routes/shopRoutes.js';
 
 const PORT = 4000;
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.static('public'));
 
 app.use('/', mainRoutes);
 app.use('/auth', authRoutes);
+app.use('/shop', shopRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server corriendo en http://localhost:${PORT}`)
