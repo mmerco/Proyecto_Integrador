20176207fa22d7283c6212e9adff85e5f53a2196
Mariano Mercogliano <mercogliano@gmail.com>
2023-11-18 19:59:01 -0300

    [FEAT] rutas y controladores para admin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mainRoutes from './src/routes/mainRoutes.js';
 import authRoutes from './src/routes/authRoutes.js';
 import shopRoutes from './src/routes/shopRoutes.js';
+import adminRoutes from './src/routes/adminRoutes.js'
 
 const PORT = 4000;
 const app = express();
@@ -11,6 +12,7 @@ app.use(express.static('public'));
 app.use('/', mainRoutes);
 app.use('/auth', authRoutes);
 app.use('/shop', shopRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server corriendo en http://localhost:${PORT}`)
---
+++ src/routes/adminRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import adminControllers from "../controllers/adminController.js";
+
+const router = express.Router();
+
+router.get('/', adminControllers.admin);
+router.get('/create', adminControllers.create);
+router.post('/create', adminControllers.createPost);
+router.get('/edit/:id', adminControllers.edit);
+router.put('/edit/:id', adminControllers.editPut);
+router.delete('/delete/:id', adminControllers.delete);
+
+
+export default router;
