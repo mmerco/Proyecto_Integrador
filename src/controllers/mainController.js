ccd1a4b6ba658f24ef97a491956e6e9a68ecf6cd
Mariano Mercogliano <mercogliano@gmail.com>
2023-11-21 00:19:13 -0300

    [FEAT] se crea vista para home

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,5 +1,15 @@
+import data from "../data/itemsData.js";
+
+
 const mainControllers = {
-    home: (req, res) => res.send('Route for Home View'),
+    home: (req, res) => res.render('home', {
+        items: data,
+        title: 'Home | Funkoshop',
+        banner_title: 'nuevos ingresos',
+        banner_text: 'Descubri el próximo Funko Pop de tu colección',
+        link_text: 'SHOP',
+        slider_title: 'últimos lanzamientos'
+    }),
     contact: (req, res) => res.send('Route for Contact View'),
     about: (req, res) => res.send('Route for About View'),
     faqs: (req, res) => res.send('Route for Faqs View')
