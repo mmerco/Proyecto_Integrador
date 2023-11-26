87f04346fcd89651bb1bd87e8564cdd0e17ee582
Mariano Mercogliano <mercogliano@gmail.com>
2023-11-26 14:32:20 -0300

    [FEAT] vista y controlador para register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,9 @@ const authControllers = {
         title: 'Login | Funkoshop'
     }),
     loginPost: (req, res) => res.send('Route for Login View POST'),
-    register: (req, res) => res.send('Route for Register View'),
+    register: (req, res) => res.render('register', {
+        title: 'Register | Funkoshop'
+    }),
     registerPost: (req, res) => res.send('Route for Register View POST'),
     logout: (req, res) => res.send('Route for Logout View')
 }
