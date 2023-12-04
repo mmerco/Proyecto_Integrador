b675dd9ecb77593751daf1db67b2ba82fa080f43
Mariano Mercogliano <mercogliano@gmail.com>
2023-12-04 18:07:25 -0300

    [FEAT] se instala y configura express-session

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import session from 'express-session';
 import mainRoutes from './src/routes/mainRoutes.js';
 import authRoutes from './src/routes/authRoutes.js';
 import shopRoutes from './src/routes/shopRoutes.js';
 import adminRoutes from './src/routes/adminRoutes.js'
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  name: 'Funkoshop',
+  cookie: { maxAge: 60000 * 15 },
+  resave: false,
+  saveUninitialized: false,
+}));
 
 
 // STATIC FOLDER
