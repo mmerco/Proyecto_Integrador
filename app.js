d8970f08ab4c7bc68ccfd0e26c9a8113daa9203b
Mariano Mercogliano <mercogliano@gmail.com>
2023-12-09 09:10:08 -0300

    [FEAT] se instala y configura method-override

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import session from 'express-session';
 import bodyParser from 'body-parser';
+import methodOverride from 'method-override';
 import mainRoutes from './src/routes/mainRoutes.js';
 import authRoutes from './src/routes/authRoutes.js';
 import shopRoutes from './src/routes/shopRoutes.js';
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 
+// MIDDLEWARES
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   name: 'Funkoshop',
@@ -17,6 +20,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
 }));
+app.use(methodOverride('_method'));
 
 
 // STATIC FOLDER
@@ -28,15 +32,14 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
-
 // ROUTES
 app.use('/', mainRoutes);
 app.use('/auth', authRoutes);
 app.use('/shop', shopRoutes);
 app.use('/admin', adminRoutes);
 
+
+// PORT
 app.listen(PORT, () =>
   console.log(`Server corriendo en http://localhost:${PORT}`)
 );
